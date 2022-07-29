326c487c28e083da5b6a6dd3ab00c2259285e9c0
AndyDHaines <andydhaines@gmail.com>
2022-07-29 18:34:27 +0100

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -9,11 +9,29 @@ fun main() {
     var playerLevel = 4
     println(playerLevel)
 
-    if (playerLevel == 1){
+    val hasBefriendedBarbarians = true
+    val hasAngeredBarbarians = false
+    val playerClass = "Paladin"
+    if (playerLevel == 1) {
         println("Meet Mr. Bubbles in the land of soft things.")
-    } else {
+    } else if (playerLevel <= 5) {
+        //Check if diplomacy is an option
+        val canTalkToBarbarians = !hasAngeredBarbarians && (hasBefriendedBarbarians || playerClass == "Barbarian")
+        if (canTalkToBarbarians) {
+            println("Convince the barbarians to call off their invasion")
+        } else {
+            println("Save the town from the barbarian invasions.")
+        }
+    } else if (playerLevel == 6) {
         println("Locate the enchanted sword.")
+    } else if (playerLevel == 7) {
+        println("Recover the long-last artifact of creation.")
+    } else if (playerLevel == 8) {
+        println("Defeat Nogartse, bringer of death and eater of worlds.")
+    } else {
+        println("There are no quests right now.")
     }
+
 
     println("Time passes.....")
     println("The hero returns from his quest")
